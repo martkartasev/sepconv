f8b95ff9d2386130b3e7b4c2503506001ebd4803
Carlo Rapisarda <rapisarda.carlo@gmail.com>
2018-05-07 20:45:04 +0200

    Add initial implementation of the dataset wrapper

--- src/dataset.py
+++ src/dataset.py
@@ -2,43 +2,97 @@
 # KTH Royal Institute of Technology
 #
 
+import torch.utils.data as data
+import torch
+from torchvision.transforms import Compose, CenterCrop, ToTensor
 from os.path import exists, join, basename
-from os import makedirs, remove
+from os import makedirs, remove, listdir
 from six.moves import urllib
-import tarfile
+from PIL import Image
+import zipfile
 
-def download_davis(dest="dataset"):
+def load_img(file_path):
+    img = Image.open(file_path).convert('RGB')
+    x, _, _ = img.split()
+    return x
 
-    output_dir = join(dest, "DAVIS/videos")
+class DatasetFromFolder(data.Dataset):
+
+    def __init__(self, image_dir, input_transform, target_transform):
+        super(DatasetFromFolder, self).__init__()
+
+        paths = listdir(image_dir)
+        tuples = []
+        for i in range(len(paths) // 3):
+            x1, t, x2 = paths[i*3], paths[i*3 +1], paths[i*3 +2]
+            x1, t, x2 = join(image_dir, x1), join(image_dir, t), join(image_dir, x2)
+            tuples.append((x1, t, x2))
+
+        self.image_tuples = tuples
+        self.input_transform = input_transform
+        self.target_transform = target_transform
+
+    def __getitem__(self, index):
+        touple = self.image_tuples[index]
+        x1, target, x2 = load_img(touple[0]), load_img(touple[1]), load_img(touple[2])
+
+        if self.input_transform:
+            x1 = self.input_transform(x1)
+            x2 = self.input_transform(x2)
+        if self.target_transform:
+            target = self.target_transform(target)
+
+        input = torch.tensor([x1, x2])
+        return input, target
+
+    def __len__(self):
+        return len(self.image_tuples)
+
+def download_davis(dest="./dataset"):
+
+    output_dir = join(dest, "DAVIS")
 
     if not exists(output_dir):
 
-        makedirs(dest)
+        if not exists(dest):
+            makedirs(dest)
 
-        url = "..." # TODO: Add actual URL
-        print("Downloading DAVIS...")
+        url = "https://graphics.ethz.ch/Downloads/Data/Davis/DAVIS-data.zip"
 
-        data = urllib.request.urlopen(url)
+        print("===> Downloading DAVIS...")
+        response = urllib.request.urlopen(url)
+        zip_path = join(dest, basename(url))
+        with open(zip_path, 'wb') as f:
+            f.write(response.read())
 
-        file_path = join(dest, basename(url))
-        with open(file_path, 'wb') as f:
-            f.write(data.read())
+        zip_ref = zipfile.ZipFile(zip_path, 'r')
 
-        print("Extracting data...")
-        with tarfile.open(file_path) as tar:
-            for item in tar:
-                tar.extract(item, dest)
+        print("===> Extracting data...")
+        zip_ref.extractall(dest)
+        zip_ref.close()
 
-        remove(file_path)
+        # Remove downloaded zip file
+        remove(zip_path)
 
     return output_dir
 
+def _input_transform(crop_size):
+    return Compose([
+        CenterCrop(crop_size),
+        ToTensor(),
+    ])
+
+def _target_transform(crop_size):
+    return _input_transform(crop_size)
+
 def get_training_set():
     root_dir = download_davis()
-    # TODO: ...
-    return None
+    jpegs_dir = join(root_dir, "JPEGImages/480p")
+    crop_size = 128
+    return DatasetFromFolder(jpegs_dir, _input_transform(crop_size), _target_transform(crop_size))
 
 def get_test_set():
     root_dir = download_davis()
-    # TODO: ...
-    return None
+    jpegs_dir = join(root_dir, "JPEGImages/480p")
+    crop_size = 128
+    return DatasetFromFolder(jpegs_dir, _input_transform(crop_size), _target_transform(crop_size))
--- src/main.py
+++ src/main.py
@@ -4,9 +4,10 @@
 
 import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader
 import torch.optim as optim
 from model import Net, CustomLoss
-from data import get_training_set, get_test_set
+from dataset import get_training_set, get_test_set
 
 
 if torch.cuda.is_available():
@@ -14,17 +15,20 @@ if torch.cuda.is_available():
     device = torch.device("cuda")
 else:
     print("===> No GPU found, proceeding with CPU...")
-	device = torch.device("cpu")
+    device = torch.device("cpu")
 
 # torch.manual_seed(42)
 
 print('===> Loading datasets...')
 train_set = get_training_set()
 test_set = get_test_set()
+training_data_loader = DataLoader(dataset=train_set, num_workers=0, batch_size=100, shuffle=True)
+testing_data_loader = DataLoader(dataset=test_set, num_workers=0, batch_size=100, shuffle=False)
+
 
 print('===> Building model...')
 model = Net().to(device)
-criterion = CustomLoss()
+criterion = nn.MSELoss()
 optimizer = optim.Adam(model.parameters())
 
 # ...
