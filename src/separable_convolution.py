bb663deabe40ac969c04b826b77a8e797b331408
Carlo Rapisarda <rapisarda.carlo@gmail.com>
2018-05-09 16:50:23 +0200

    Make SeparableConvolutionSlow differentiable

--- src/separable_convolution.py
+++ src/separable_convolution.py
@@ -3,7 +3,7 @@ import torch as t
 from src.config import OUTPUT_1D_KERNEL_SIZE as FILTER_SIZE
 
 
-class SeparableConvolutionSlow(t.autograd.Function):
+class SeparableConvolutionSlow(t.nn.Module):
     def __init__(self):
         super(SeparableConvolutionSlow, self).__init__()
     
@@ -18,14 +18,11 @@ class SeparableConvolutionSlow(t.autograd.Function):
         assert vertical.size(1) == horizontal.size(1) == FILTER_SIZE
         assert vertical.size(2) == horizontal.size(2) == vertical.size(3) == horizontal.size(3) == m_out
         
-        output = input.new().resize_(n_b, n_channels, m_out, m_out).zeros_()
+        output = im.new().resize_(n_b, n_channels, m_out, m_out).zero_()
         
         for b in range(n_b):
              local_separable_conv_2d(im[b], horizontal[b], vertical[b], output=output[b])
         return output
-    
-    def backward(self, grad_output):
-        raise NotImplementedError
 
 
 def local_separable_conv_2d(im, horizontal, vertical, output=None):
@@ -40,6 +37,6 @@ def local_separable_conv_2d(im, horizontal, vertical, output=None):
         for col in range(m_out):
             sub_patch = im[:, row:row + FILTER_SIZE, col:col + FILTER_SIZE]
             local_horiz = horizontal[:, row, col]
-            local_vert = vertical[:, row, col].reshape(-1, 1)
+            local_vert = vertical[:, row, col].view(-1, 1)
             output[:, row, col] = (sub_patch * local_horiz * local_vert).sum(dim=1).sum(dim=1)
     return output
