4071c6d9368c834a38783088df026852818455c2
Carlo Rapisarda <rapisarda.carlo@gmail.com>
2018-05-14 23:22:13 +0200

    Disable cropping when interpolating frames

--- src/interpolate.py
+++ src/interpolate.py
@@ -14,7 +14,7 @@ from src.config import CROP_SIZE
 _center_crop = CenterCrop(CROP_SIZE)
 
 def _img_trasform(img):
-    img = _center_crop(img)
+    # img = _center_crop(img)
     # MUST mimic the behavior of pil_transform() in dataset.py
     return torch.from_numpy(np.rollaxis(np.asarray(img) / 255.0, 2)).float()
 
@@ -51,7 +51,7 @@ if __name__ == '__main__':
 
     from src.model import Net
 
-    parser = argparse.ArgumentParser(description='Video Frame Interpolation')
+    parser = argparse.ArgumentParser(description='Frame Interpolation')
     parser.add_argument('--prev', type=str, required=True, help='path to frame at t-1')
     parser.add_argument('--succ', type=str, required=True, help='path to frame at t+1')
     parser.add_argument('--dest', type=str, required=True, help='output path of the resulting frame at t')
