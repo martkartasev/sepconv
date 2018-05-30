47ee857dbb48527344940e055822b2abac5d14c1
Carlo Rapisarda <rapisarda.carlo@gmail.com>
2018-05-30 16:12:41 +0200

    Refactor

--- src/stitch_frames.py
+++ src/stitch_frames.py
@@ -1,3 +1,6 @@
+#
+# KTH Royal Institute of Technology
+#
 
 import argparse
 from timeit import default_timer as timer
@@ -7,32 +10,32 @@ from src.interpolate_video import _write_video
 from src.data_manager import is_image, load_img
 
 
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser(description='Frame to video')
-    parser.add_argument('--src', type=str, required=True, help='path to the video directory')
-    parser.add_argument('--dest', type=str, required=True, help='path to the output file')
-    parser.add_argument('--outputfps', type=int, required=False, default=None, help='frame-rate of the output')
-    parser.add_argument('--dropframes', type=bool, required=False, default=False, help='if every other frame should be dropped')
-    params = parser.parse_args()
-
-    root_path = params.src
+def stitch_frames(src_path, dest_path, output_fps=None, drop_frames=False):
 
     tick_t = timer()
 
-    frames = [join(root_path, x) for x in listdir(root_path)]
+    frames = [join(src_path, x) for x in listdir(src_path)]
     frames = [x for x in frames if is_image(x)]
     frames.sort()
 
-    frames = [load_img(x) for i, x in enumerate(frames)]
-
-    if params.dropframes:
-        frames = [x for i, x in enumerate(frames) if i%2 == 0]
+    print('===> Loading frames...')
+    if drop_frames:
+        frames = [load_img(x) for i, x in enumerate(frames) if i % 2 == 0]
+    else:
+        frames = [load_img(x) for i, x in enumerate(frames)]
 
     print('===> Writing results...')
-    _write_video(params.dest, frames, params.outputfps)
+    _write_video(dest_path, frames, output_fps)
 
     tock_t = timer()
-
     print("Done. Took ~{}s".format(round(tock_t - tick_t)))
 
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Frames to video file')
+    parser.add_argument('--src', dest='src_path', type=str, required=True, help='path to the directory containing the frames')
+    parser.add_argument('--dest', dest='dest_path', type=str, required=True, help='path to the output file')
+    parser.add_argument('--outputfps', dest='output_fps', type=int, required=False, default=None, help='frame-rate of the output')
+    parser.add_argument('--dropframes', dest='drop_frames', type=bool, required=False, default=False, help='whether or not every other frame should be dropped')
+    stitch_frames(**vars(parser.parse_args()))
